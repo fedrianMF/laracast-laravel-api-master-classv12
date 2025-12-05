ff6010ed58a1f2b4536a9126edf5a40d595b41df
Felix Mamani <fedrian.mamani@gmail.com>
2025-12-04 22:57:04 -0400

    feat: Add API v1 for ticket resource management including controller, requests, routes, and basic authentication routes.

--- routes/api.php
+++ routes/api.php
@@ -3,14 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-use App\Models\Ticket;
 use App\Http\Controllers\AuthController;
 
 Route::post('/login', [AuthController::class, 'login']);
-
-Route::get('/tickets', function () {
-    return Ticket::all();
-});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
