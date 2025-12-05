62fd1d0a03c3b2272a273ae43e6e959e50a273d5
Felix Mamani <fedrian.mamani@gmail.com>
2025-12-04 22:26:23 -0400

    feat: Implement initial API setup including authentication, ticket resource, and standardized API responses.

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Models\Ticket;
+use App\Http\Controllers\AuthController;
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/tickets', function () {
+    return Ticket::all();
+});
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
